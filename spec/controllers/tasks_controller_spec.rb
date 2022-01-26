57d72497040d40006992afef2535de8f330ab11a
claudiahalip <claudia4ro@yahoo.com>
2022-01-26 16:20:34 -0600

    fix spaces

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -4,19 +4,19 @@ require 'json'
 RSpec.describe TasksController, type: :controller do
 
     describe "POST#create" do
-        it "returns http success " do
+        it "returns http success" do
             post :create, params: { task: {:description => "test1"}}
             expect(response).to have_http_status(:success)
         end
 
-        it "returns an error if the task is not valid " do
+        it "returns an error if the task is not valid" do
             post :create, params: { task: {:description => nil}}
             expect(JSON.parse(response.body)).to eq("error" => "The task couldn't be added. Try again!")
         end
     end 
 
     describe "GET#index" do
-        it "returns http success " do
+        it "returns http success" do
             get :index
             expect(response).to have_http_status(:success)
         end
