4ec8103364e8f17ca4ff64f39d89c63c95aa3cb2
claudiahalip <claudia4ro@yahoo.com>
2022-02-04 09:56:56 -0600

    refatored task scheduler, added test

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,8 +5,11 @@ class Task < ApplicationRecord
 
     scheduler = Rufus::Scheduler.new
 
-    scheduler.every '300s' do
-        Task.all.each { |task_obj| task_obj.update(is_completed: false) if task_obj.is_completed == true} 
+    def self.update_task
+        Task.where('is_completed = ?', true).update_all(is_completed: false )
     end
 
+    scheduler.every '300s' do
+        update_task 
+    end
 end
